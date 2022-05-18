628f7a624645d36c3dd4e3fe0d4e2914408833c6
VikashPR <vikashrajendran2806@gmail.com>
2022-05-18 17:02:35 +0530

    Feat: npm init

---
+++ script.js
@@ -0,0 +1,5 @@
+export function getAboutUsLink(language){switch (language.toLowerCase()){ case englishCode.toLowerCase():   return '/about-us'; case spanishCode.toLowerCase():
+        return '/acerca-de';
+    }
+    return '';
+  }
