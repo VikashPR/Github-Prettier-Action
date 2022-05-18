23eff6b752caea4064f0a239026161e0b7354808
VikashPR <VikashPR@users.noreply.github.com>
2022-05-18 11:59:47 +0000

    Prettified Code!

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-export function getAboutUsLink(language){switch (language.toLowerCase()){ case englishCode.toLowerCase():   return '/about-us'; case spanishCode.toLowerCase():
-        return '/acerca-de';
-    }
-    return '';
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
   }
+  return "";
+}
