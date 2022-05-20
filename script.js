8a815862d1bcb5db13fa023be12489fc013ed970
VikashPR <vikashrajendran2806@gmail.com>
2022-05-20 14:46:29 +0530

    Test: test 2

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 export function getAboutUsLink(language) {
   switch (language.toLowerCase()) {
-    case englishCode.toLowerCase():
-      return "/about-us";
-    case spanishCode.toLowerCase():
-      return "/acerca-de";
+    case englishCode.toLowerCase():return "/about-us";
+    case spanishCode.toLowerCase():return "/acerca-de";
   }
   return "";
+}
+export function getAboutUsLink(language) {switch (language.toLowerCase()) {case englishCode.toLowerCase():return "/about-us";case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }return "";
 }
