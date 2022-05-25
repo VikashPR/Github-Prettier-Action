6cd53e7055d5f56158b4a7d0535c3261e7363850
VikashPR <vikashrajendran2806@gmail.com>
2022-05-25 14:34:06 +0530

    Test: test run 1

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-export function getAboutUsLink(language) {
-  switch (language.toLowerCase()) {
-    case englishCode.toLowerCase():
-      return "/about-us";
-    case spanishCode.toLowerCase():
-      return "/acerca-de";
-  }
-  return "";
-}
-export function getAboutUsLink(language) {
-  switch (language.toLowerCase()) {
-    case englishCode.toLowerCase():
-      return "/about-us";
-    case spanishCode.toLowerCase():
-      return "/acerca-de";
-  }
-  return "";
-}
+            export function getAboutUsLink(language)                    {switch (language.toLowerCase()) {case                                                                  englishCode.toLowerCase(): return "/about-us";
+                    case spanishCode.toLowerCase()                                                                                      : return "/acerca-de";}return "";}
+                      export function getAboutUsLink(language) {switch (language.toLowerCase()) {
+                                                            case englishCode.toLowerCase():return "/about-us";
+                        case spanishCode.toLowerCase(): return "/acerca-de";}return "";
+                  }
+
+                      class                         MarketContainer extends Component {
+  // code omitted for brevity
+                        }
+ 
+              const mapDispatchToProps = dispatch => {
+           return {
+                updateSummary: (summary, start, today) => dispatch(updateSummary(summary, start, today))}}
+ 
+                    export default connect(null, mapDispatchToProps)(MarketContainer);
+          export function createConnect(                                      {connectHOC = connectAdvanced,
+                      mapStateToPropsFactories 
+                = defaultMapStateToPropsFactories,                                                    mapDispatchToPropsFactories 
+               =                                      defaultMapDispatchToPropsFactories,mergePropsFactories = 
+            defaultMergePropsFactories,
+            selectorFactory 
+              = 
+               defaultSelectorFactory
+                      })
+  
