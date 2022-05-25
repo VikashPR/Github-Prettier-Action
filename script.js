51d3f25763290a8a5b1b3af6fb50449b403356dd
VikashPR <VikashPR@users.noreply.github.com>
2022-05-25 09:08:24 +0000

    Prettified Code!

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-            export function getAboutUsLink(language)                    {switch (language.toLowerCase()) {case                                                                  englishCode.toLowerCase(): return "/about-us";
-                    case spanishCode.toLowerCase()                                                                                      : return "/acerca-de";}return "";}
-                      export function getAboutUsLink(language) {switch (language.toLowerCase()) {
-                                                            case englishCode.toLowerCase():return "/about-us";
-                        case spanishCode.toLowerCase(): return "/acerca-de";}return "";
-                  }
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
 
-                      class                         MarketContainer extends Component {
+class MarketContainer extends Component {
   // code omitted for brevity
-                        }
+}
