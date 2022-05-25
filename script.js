5bb9de615efe7f2b82367c40849937b4b4a8f2f9
VikashPR <vikashrajendran2806@gmail.com>
2022-05-25 14:38:06 +0530

    Test: test run 5

--- script.js
+++ script.js
@@ -8,10 +8,3 @@
                       class                         MarketContainer extends Component {
   // code omitted for brevity
                         }
- 
-              const mapDispatchToProps = dispatch => {
-           return {
-                updateSummary: (summary, start, today) => dispatch(updateSummary(summary, start, today))}}
- 
-                    export default connect(null, mapDispatchToProps)(MarketContainer);
-          export function createConnect({connectHOC = connectAdvanced,mapStateToPropsFactories = defaultMapStateToPropsFactories,mapDispatchToPropsFactories =defaultMapDispatchToPropsFactories,mergePropsFactories = defaultMergePropsFactories,selectorFactory = defaultSelectorFactory})
