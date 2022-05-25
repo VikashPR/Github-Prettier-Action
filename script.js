24f3b00cb73705e25f6b75d01a3f7299307cb199
VikashPR <VikashPR@users.noreply.github.com>
2022-05-25 09:20:36 +0000

    Prettified Code!

--- script.js
+++ script.js
@@ -1,60 +1,132 @@
-            export function getAboutUsLink(language)                    {switch (language.toLowerCase()) {case                                                                  englishCode.toLowerCase(): return "/about-us";
-                    case spanishCode.toLowerCase()                                                                                      : return "/acerca-de";}return "";}
-                      export function getAboutUsLink(language) {switch (language.toLowerCase()) {
-                                                            case englishCode.toLowerCase():return "/about-us";
-                        case spanishCode.toLowerCase(): return "/acerca-de";}return "";
-                  }
-
-                      class                         MarketContainer extends Component {
-  // code omitted for brevity
-                        }
-export function getAboutUsLink(language)                    {switch (language.toLowerCase()) {case                                                                  englishCode.toLowerCase(): return "/about-us";
-case spanishCode.toLowerCase()                                                                                      : return "/acerca-de";}return "";}
-                          export function getAboutUsLink(language)                          {switch (language.toLowerCase()) {
-                                                                                        case englishCode.toLowerCase():return "/about-us";
-    case spanishCode.toLowerCase(): return "/acerca-de";}return "";
-                      }
-    
-                          class                         MarketContainer extends Component {
-// code is not present for brevity
-                            }
-                            export function getAboutUsLink(language)                    {switch (language.toLowerCase()) {case                                                                  englishCode.toLowerCase(): return "/about-us";
-case spanishCode.toLowerCase()                                                                                      : return "/acerca-de";}return "";}
-                                export function getAboutUsLink(language) {switch (language.toLowerCase()) {
-                                                                    case englishCode.toLowerCase():return "/about-us";
-                                case spanishCode.toLowerCase(): return "/acerca-de";}return "";
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
 }
-        
-                              class                         MarketContainer extends Component {
-          // code omitted for brevity
-                                }
-                                export function getAboutUsLink(language)                    {switch (language.toLowerCase()) {case                                                                  englishCode.toLowerCase(): return "/about-us";
-                                case spanishCode.toLowerCase()                                                                                      : return "/acerca-de";}return "";}
-                                  export function getAboutUsLink(language) {switch (language.toLowerCase()) {
-                                                                        case englishCode.toLowerCase():return "/about-us";
-                                    case spanishCode.toLowerCase(): return "/acerca-de";}return "";
-                              }
-            
-                                  class                         MarketContainer extends Component {
-                                    // code is not present for brevity
-                                    }
-                                    export function getAboutUsLink(language)                    {switch (language.toLowerCase()) {case                                                                  englishCode.toLowerCase(): return "/about-us";
-                                    case spanishCode.toLowerCase()                                                                                      : return "/acerca-de";}return "";}
-                                      export function getAboutUsLink(language) {switch (language.toLowerCase()) {
-                                                                            case englishCode.toLowerCase():return "/about-us";
-                                        case spanishCode.toLowerCase(): return "/acerca-de";}return "";
-                                  }
-                
-                                      class                         MarketContainer extends Component {
-                  // code omitted for brevity
-                                        }
-                                        export function getAboutUsLink(language)                    {switch (language.toLowerCase()) {case                                                                  englishCode.toLowerCase(): return "/about-us";
-                                        case spanishCode.toLowerCase()                                                                                      : return "/acerca-de";}return "";}
-                                          export function getAboutUsLink(language) {switch (language.toLowerCase()) {
-                                                                                case englishCode.toLowerCase():return "/about-us";
-                                            case spanishCode.toLowerCase(): return "/acerca-de";}return "";
-                                      }
-                    
-                                          class                         MarketContainer extends Component {
-                                            // code is not present for brevity
-                                            }
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+
+class MarketContainer extends Component {
+  // code omitted for brevity
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+
+class MarketContainer extends Component {
+  // code is not present for brevity
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+
+class MarketContainer extends Component {
+  // code omitted for brevity
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+
+class MarketContainer extends Component {
+  // code is not present for brevity
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+
+class MarketContainer extends Component {
+  // code omitted for brevity
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
+}
+
+class MarketContainer extends Component {
+  // code is not present for brevity
+}
