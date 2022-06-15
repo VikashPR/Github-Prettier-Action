9f2dc774f58be29551747a522a5349356ac5d0c0
VikashPR <vikashrajendran2806@gmail.com>
2022-06-15 14:09:21 +0530

    Test: test run

--- src/main.js
+++ src/main.js
@@ -1,36 +1,32 @@
 function App() {
   var container = document.getElementById("container");
 
-  // get the shape elements
-  var circle = document.getElementById("circle");
-  var square = document.getElementById("square");
-  var triangle = document.getElementById("triangle");
-  var pentagon = document.getElementById("pentagon");
+                  // get the shape elements
+               var                                                            circle =                                           document.getElementById("circle");
+  var square           =                                document.getElementById("square");
+  var triangle                                    = document.getElementById("triangle");
+  var pentagon                                                      = document.getElementById("pentagon");
 
   // animate function
-  function animate(elem, startX, startY, bounce, duration) {
-    // set and store the start positions (they'll need to be the same for the end position too)
-    let posStartX;
-    if (startX === "left") {
-      posStartX = 0;
-    } else {
-      posStartX = 100;
+                                      function animate(elem, startX, startY, bounce, duration) {
+    // set and store the start positions (they'll need to be the same for the end position too)let posStartX;
+                                                  if (startX === "left") {posStartX = 0;} else { posStartX = 100;
     }
 
     // posX is the X position variable we'll update
-    let posX = startX;
+                let posX = startX;
 
     // array for storing the keyframes
-    let keyframes = [];
+                     let keyframes = [];
 
     // we're going to push a transform into the keyframes array for each bounce
-    for (let i = 1; i <= bounce; i++) {
-      let translateX;
-      let translateY;
-      if (i === 1 || i === bounce) {
+                                              for (let i = 1; i <= bounce; i++) {
+                  let                                                                            translateX;
+                                    let                         translateY;
+                      if (i === 1 || i === bounce) {
         // if we're on the right side we need to do an additional translate to account for the size of the shape div
-        translateX =
-          startX === "right"
+                                               translateX =
+                                 startX === "right"
             ? `translateX(${posStartX}vw)translateX(-180px)`
             : `translateX(${posStartX}vw)`;
         translateY = `translateY(${startY}px)`;
@@ -47,26 +43,26 @@ function App() {
       }
 
       // store the transformation
-      let transform = `${translateX} ${translateY}`;
+                                                let transform = `${translateX} ${translateY}`;
 
-      keyframes.push({ transform: transform });
+                  keyframes.push({ transform: transform });
 
       // change the X position
       posX = posX === "right" ? "left" : "right";
     }
     var options = {
-      duration: duration,
-      iterations: Infinity,
-    };
-    elem.animate(keyframes, options);
-  }
+                  duration: duration,
+                      iterations: Infinity,
+                };
+                          elem.animate(keyframes, options);
+                              }
 
-  // animate!
-  animate(circle, "left", 12, 5, 36000);
+            // animate!
+                  animate(circle, "left", 12, 5, 36000);
 
-  animate(square, "left", 60, 7, 37000);
-  animate(triangle, "right", 27, 5, 38000);
-  animate(pentagon, "right", 99, 7, 39000);
+                                                                                  animate(square, "left", 60, 7, 37000);
+              animate(triangle, "right", 27, 5, 38000);
+                                                    animate(pentagon, "right", 99, 7, 39000);
 }
 
 App();
