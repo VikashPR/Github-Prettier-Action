ded29012fe488391505db9db001be35b967faa0f
VikashPR <VikashPR@users.noreply.github.com>
2022-06-15 08:40:11 +0000

    Prettified Code!

--- src/script.js
+++ src/script.js
@@ -1,36 +1,36 @@
-export function getAboutUsLink(language)                            {
-              switch (language.toLowerCase())                {
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
     case englishCode.toLowerCase():
-                              return "/about-us";
-              case spanishCode.toLowerCase():
-                  return "/acerca-de";
-  }
-           return "";
-}
-export function                                                                    getAboutUsLink(language) {
-  switch                                                        (language.toLowerCase()) {
-                  case                                        englishCode.toLowerCase():
-                   return "/about-us";
-                                          case                                          spanishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
       return "/acerca-de";
   }
   return "";
 }
-export                            function                          getAboutUsLink(language) {
-  switch                (language.toLowerCase())                                         {
-    case                englishCode.toLowerCase():
-      return             "/about-us";
-    case                spanishCode.toLowerCase():
-                            return "/acerca-de";
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
   }
-                           return "";
+  return "";
 }
-export                                     function                                     getAboutUsLink(language) {
-  switch                            (language.toLowerCase()) {
-    case                   englishCode.toLowerCase():
-      return                           "/about-us";
-    case                  spanishCode.toLowerCase():
-      return                     "/acerca-de";
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
   }
-                                                    return "";
+  return "";
+}
+export function getAboutUsLink(language) {
+  switch (language.toLowerCase()) {
+    case englishCode.toLowerCase():
+      return "/about-us";
+    case spanishCode.toLowerCase():
+      return "/acerca-de";
+  }
+  return "";
 }
