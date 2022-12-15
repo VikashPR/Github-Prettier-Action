18cf5045596e552c67187096e060b8674b1aa600
VikashPR <vikashrajendran2806@gmail.com>
2022-12-15 19:04:57 +0530

    chore: test

--- src/main.js
+++ src/main.js
@@ -10,6 +10,15 @@ function App() {
   // animate function
   function animate(elem, startX, startY, bounce, duration) {
     // set and store the start positions (they'll need to be the same for the end position too)let posStartX;
+
+
+
+
+
+
+
+
+    
     if (startX === "left") {
       posStartX = 0;
     } else {
